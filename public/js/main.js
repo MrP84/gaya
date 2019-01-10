14f17fe1779f59b44697110c1a90632862563085
MrP <gllmprvnzn@gmail.com>
2019-01-10 10:52:50 +0100

    même fonction, version jquery

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,6 @@
-var burgerBtn = document.getElementById('burgerBtn');
-var mobile = document.getElementById('mobile');
-var demo1 = document.getElementById('demo1');
-var demo2 = document.getElementById('demo2');
-var demo3 = document.getElementById('demo3');
+var burgerBtn = $('#burgerBtn');
+var mobile = $('#mobile');
 
-burgerBtn.addEventListener('click', function() {
-  mobile.classList.toggle('navigation');
-}, false);
+burgerBtn.on('click', () => {
+  mobile.toggleClass('navigation');
+});
